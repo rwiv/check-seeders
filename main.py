d9c141f81708c121a57bbfc25712bd72a8c9b6c4
Jongho Kim <zc798546@gmail.com>
2023-03-26 19:46:15 +0900

    fix: fix "syntax error: unterminated quoted string"

--- main.py
+++ main.py
@@ -2,48 +2,78 @@ import os
 import subprocess
 import json
 
+
 def main():
     if not os.path.exists("./output"):
         os.mkdir("./output")
 
     filenames = os.listdir("./torrents")
+    result, debug = get_result(filenames)
 
     with open("./output/result.json", "w") as file:
-        result = getResult(filenames)
         file.write(json.dumps(result, indent=4))
 
-def getResult(filenames):
-    def toInfo(filename):
-        lines = execTransmission(filename)
-        return {
+    with open("./output/debug.json", "w") as file:
+        file.write(json.dumps(debug, indent=4))
+
+
+def get_result(filenames):
+    result = []
+    debug = []
+
+    for filename in filenames:
+        stdout, stderr = exec_transmission(filename)
+
+        if len(stderr) > 0:
+            raise Exception("exec error!", stderr)
+        else:
+            print(f"{filename} finished!")
+
+        result.append({
             "filename": filename,
-            "trackers": filterError(getTrackers(lines))
-        }
+            "trackers": filter_error(get_trackers(stdout))
+        })
 
-    return list(map(toInfo, filenames))
+        debug.append({
+            "filename": filename,
+            "stdout": stdout,
+            "stderr": stderr
+        })
 
-def execTransmission(filename):
-    cmd = f"transmission-show -s './torrents/{filename}'"
-    return subprocess\
-        .check_output(cmd, shell=True, encoding='utf-8')\
-        .split("\n")
+    return result, debug
 
-def getTrackers(lines):
-    def isTracker(line):
+
+def exec_transmission(filename):
+    p = subprocess.Popen(
+        f'transmission-show -s "./torrents/{filename}"',
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8"
+    )
+
+    return list(p.stdout), list(p.stderr)
+
+
+def get_trackers(lines):
+    def is_tracker(line):
         return line.find(" ... ") != -1
 
-    def toTrackerObj(line):
+    def to_tracker_obj(line):
         chunks = line.split(" ... ")
         return {
             "url": chunks[0],
             "state": chunks[1]
         }
 
-    return list(map(toTrackerObj, filter(isTracker, lines)))
+    return list(map(to_tracker_obj, filter(is_tracker, lines)))
 
-def filterError(trackers):
-    isNotError = lambda tracker: tracker["state"].find("error") == -1
-    return list(filter(isNotError, trackers))
+
+def filter_error(trackers):
+    def is_not_error(tracker):
+        return tracker["state"].find("error") == -1
+
+    return list(filter(is_not_error, trackers))
 
 
 main()
